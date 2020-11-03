a7f331ca2514d2d86c2b13938020c0e999a1feca
Pepo <pepo@uprtcl.io>
2020-11-03 22:16:03 +0100

    first commit === 0.0.1-alpha.76

---
+++ evees-ethereum/src/uprtcl-evees-ethereum.ts
@@ -0,0 +1,6 @@
+// Required by inversify
+import 'reflect-metadata';
+
+export { EveesEthereumConnection } from './provider/evees.ethereum.connection';
+export { EveesEthereumModule } from './evees-ethereum.module';
+export { EthereumOrbitDBIdentity } from './orbitdb/ethereum.orbtidb.identity';
